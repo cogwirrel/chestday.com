2d9410313cf0d2b75ae2fb5bd7aa7e5d0361e3e8
Jack Stevenson <cogwirrel@gmail.com>
2013-01-14 22:58:23 +0000

    Initial commit of current website

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ chestday.com
 ============
 
 The website chestday.com
+
+Objective of the site is to inform the user whether or not it is Chest Day. Chest Day is on a Monday.
---
+++ js/chest.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  date = new Date();
+  day = date.getDay();
+  if(day == 1) {
+    setChest("Chest Day", "It's Chest Day", "Go train Chest!", "arnie");
+  } else {
+    setChest("Not Chest Day", "It's not Chest Day", "Go train an inferior muscle group", "crying");
+  }
+});
+
+setChest = function (title, header, footer, img) {
+  document.title = title;
+  $('#chest-header').html(header);
+  $('#chest-img').attr('src', "img/" + img + ".jpg");
+  $('#chest-footer').html(footer);
+}
