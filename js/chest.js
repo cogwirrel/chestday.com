49690ed369c3dfb2900d7f64cbb121da1168e1dc
Jack Stevenson <cogwirrel@gmail.com>
2013-10-12 21:26:22 +0100

    Replace fixed footer message with randomly chosen message.

--- js/chest.js
+++ js/chest.js
@@ -7,6 +7,11 @@ random = function (lower, upper) {
   return lower + Math.floor(Math.random()*(upper + 1 - lower))
 }
 
+/* Returns a random element from the given array */
+randomElement = function (array) {
+  return array[ random(0, array.length - 1) ];
+}
+
 setChest = function (title, header, footer, img) {
   document.title = title;
   $('#chest-header').html(header);
@@ -18,8 +23,36 @@ $(document).ready(function() {
   date = new Date();
   day = date.getDay();
   if(day == 1) {
-    setChest("Chest Day", "It's Chest Day.", "Go train Chest!", randomImage("yes_", 1, 6));
+    setChest("Chest Day", "It's Chest Day.", chestDayFooter(), randomImage("yes_", 1, 6));
   } else {
-    setChest("Not Chest Day", "It's not Chest Day.", "Go train an inferior muscle group.",  randomImage("no_", 1, 7));
+    setChest("Not Chest Day", "It's not Chest Day.", notChestDayFooter(),  randomImage("no_", 1, 7));
   }
 });
+
+chestDayFooter = function () {
+  return randomElement([
+    "Go train chest!",
+    "What are you waiting for?!",
+    "Hit the gym!",
+    "Get on the bench now!",
+    "Why aren't you on the bench?!",
+    "You should be under a barbell right now.",
+    "Time to pump iron!",
+    "Smash it!",
+    "Pump it like Arnold!",
+    "Why aren't you under a barbell?!",
+    "Go hard or go home!"
+    ]);
+}
+
+notChestDayFooter = function () {
+  return randomElement([
+    "Go train an inferior muscle group.",
+    "Maybe train legs? ...Nah",
+    "It's not worth going.",
+    "Don't even bother.",
+    "Just go home.",
+    "Go back to bed.",
+    "What's the point?"
+    ]);
+}
