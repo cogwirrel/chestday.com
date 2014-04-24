253725ccfb38f38e30e9ae359b3a605e561b486d
Jack Stevenson <cogwirrel@gmail.com>
2014-04-24 21:50:17 +0100

    Add Razor sharp peakin track

--- js/chest.js
+++ js/chest.js
@@ -144,6 +144,7 @@ var days = [
 			"102313961", // Calvin Harris - Eat Sleep Rave Repeat
 			"122455506", // Tiesto - Red Lights
 			"106914652", // DVBBS & Borgeous - Tsunami
+			"90499890", // Pegboard Nerds & Tristam - Razor Sharp
 			"74396084" // Virtual Riot - Idols (EDM Mashup)
 			// "77471974", // 2-second test track
 			// "65555191" // 10-second test track
