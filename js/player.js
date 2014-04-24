1bd881d3e86da2d26314cbd92b8166825f510c6e
Jack Stevenson <cogwirrel@gmail.com>
2014-04-25 00:28:10 +0100

    Add input to player for soundcloud id. Also when enter track in url it will play that first, then continue with normal peakin tracks

--- js/player.js
+++ js/player.js
@@ -4,18 +4,25 @@ $(document).ready(function() {
 
 	var trackParam = getParameterByName("track");
 
-	if(trackParam !== "") {
-		tracks.push(trackParam);
-	} else {
-		for(var i = 0; i < days.length; i++) {
-			if(days[i].title == "Chest Day") {
-				tracks = days[i].tracks;
-			}
+	for(var i = 0; i < days.length; i++) {
+		if(days[i].title == "Chest Day") {
+			tracks = days[i].tracks;
 		}
 	}
 
 	// Attach peakinTrackPlayer to window ( skip button uses this global reference :/ )
 	window.peakinTrackPlayer = new PeakinTrackPlayer('peakin-track-container', tracks, function() {
+		if(trackParam !== "") {
+			window.peakinTrackPlayer.switchTracks(trackParam);
+		}
+		
 		$('#headphone-button').click();
 	});
 });
+
+setTrack = function() {
+	var track = $('#track-input').val();
+	if(!isNaN(track)) {
+		window.peakinTrackPlayer.switchTracks(track);
+	}
+}
